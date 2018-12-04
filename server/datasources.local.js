e0712a145e3629e38517e0872127c805e2f5bda5
Hugo Fialho <hugofialho.hf@gmail.com>
2018-12-04 16:52:08 -0200

    first commit

---
+++ server/datasources.local.js
@@ -0,0 +1,15 @@
+// Copyright IBM Corp. 2014. All Rights Reserved.
+// Node module: loopback-example-offline-sync
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = {
+    pallas: {
+      connector: 'mysql',
+      hostname: process.env.RDS_HOSTNAME || 'localhost',
+      port:     process.env.RDS_PORT     || 3306,
+      user:     process.env.RDS_USERNAME || 'root',
+      password: process.env.RDS_PASSWORD || 'P$f130',
+      database: process.env.RDS_DB_NAME  || 'sispallas',
+    }
+  };
